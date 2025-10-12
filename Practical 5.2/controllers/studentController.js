04eabb1b58e57bba208c9a3e728f00b8a9b55bd4
Ayush Verma <157206215+ayushverma03@users.noreply.github.com>
2025-10-12 20:22:18 +0530

    Create studentController.js

---
+++ Practical 5.2/controllers/studentController.js
@@ -0,0 +1,55 @@
+const Student = require("../models/studentModel");
+
+
+exports.createStudent = async (req, res) => {
+  try {
+    const student = new Student(req.body);
+    const saved = await student.save();
+    res.status(201).json(saved);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+
+exports.getAllStudents = async (req, res) => {
+  try {
+    const students = await Student.find();
+    res.json(students);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+
+exports.getStudentById = async (req, res) => {
+  try {
+    const student = await Student.findById(req.params.id);
+    if (!student) return res.status(404).json({ message: "Student not found" });
+    res.json(student);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+
+exports.updateStudent = async (req, res) => {
+  try {
+    const updated = await Student.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.json(updated);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+
+exports.deleteStudent = async (req, res) => {
+  try {
+    await Student.findByIdAndDelete(req.params.id);
+    res.json({ message: "Student deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
