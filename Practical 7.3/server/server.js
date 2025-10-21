0e773b9fcdb1259ca4275bdfa237cddcad9c0314
Ayush Verma <157206215+ayushverma03@users.noreply.github.com>
2025-10-21 17:56:15 +0530

    Create server.js

---
+++ Practical 7.3/server/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  // receive message from one client
+  socket.on("chat message", (msg) => {
+    console.log("Message:", msg);
+    // broadcast to all clients
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(5000, () => {
+  console.log("Server running on http://localhost:5000");
+});
