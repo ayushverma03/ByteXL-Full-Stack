b7d25c93620627ec926751e79391c25fd792e29b
Ayush Verma <157206215+ayushverma03@users.noreply.github.com>
2025-10-30 12:28:08 +0530

    Create App.jsx

---
+++ Practical 9.1/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Fullstack 9.1 - Dockerized React App</h1>
+      <p>Production-ready Docker image using multi-stage build.</p>
+    </div>
+  );
+};
+
+export default App;
