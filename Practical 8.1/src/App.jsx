320280f4cf1e2b5c382b3a2b1e8d3f9f2b59dd57
Ayush Verma <157206215+ayushverma03@users.noreply.github.com>
2025-10-30 12:03:49 +0530

    Create App.jsx

---
+++ Practical 8.1/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoginForm from './components/LoginForm';
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <h1>Welcome to Fullstack 8.1</h1>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default App;
