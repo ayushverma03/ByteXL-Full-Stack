e784fed27065dd6e08abdae5ee72def1f3d4a5a2
Ayush Verma <157206215+ayushverma03@users.noreply.github.com>
2025-10-30 12:39:35 +0530

    Create index.js

---
+++ Practical 9.3/backend/server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const apiRoutes = require('./routes/api');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api', apiRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Backend is running!');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
