d4ab21f1eace2801e54f57243bc23f3781378c93
Ayush Verma <157206215+ayushverma03@users.noreply.github.com>
2025-10-30 12:21:57 +0530

    Create authMiddleware.js

---
+++ Practical 8.3/server/middleware/authMiddleware.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = "your_secret_key";
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) return res.status(401).json({ message: "Access denied. No token provided." });
+
+  try {
+    const decoded = jwt.verify(token, SECRET_KEY);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(403).json({ message: "Invalid or expired token." });
+  }
+};
+
+module.exports = { verifyToken };
