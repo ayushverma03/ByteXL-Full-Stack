fc5ba740a6b4d13d1ba080497f796e0f38f45fb0
Ayush Verma <157206215+ayushverma03@users.noreply.github.com>
2025-11-03 10:29:33 +0530

    Create server.js

---
+++ Practical 10.3/backend/server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const postRoutes = require("./routes/postRoutes");
+const commentRoutes = require("./routes/commentRoutes");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Connect to DB
+connectDB();
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
