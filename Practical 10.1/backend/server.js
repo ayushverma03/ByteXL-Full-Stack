090515ec36bb5d3f1f5eaca2bf6d865da24ebfdb
Ayush Verma <157206215+ayushverma03@users.noreply.github.com>
2025-11-03 09:57:30 +0530

    Create server.js

---
+++ Practical 10.1/backend/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const studentRoutes = require("./routes/studentRoutes");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api/students", studentRoutes);
+
+mongoose.connect("mongodb://127.0.0.1:27017/studentDB")
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.listen(5000, () => console.log("Server running on port 5000"));
